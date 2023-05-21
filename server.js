4938bb62c3ed6beca0fe04ee6828e0c8de25c57d
DawitSishu <dawitalemayhu55@gmail.com>
2023-05-21 02:48:03 -0400

    implemented cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const connectToDb = require('./config/connectDb');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config()
+var cors = require('cors')
 const app = express()
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.json())
 connectToDb()
 
